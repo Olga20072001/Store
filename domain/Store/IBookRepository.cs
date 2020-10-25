1c6e5497cf82522f4f8fad2bf9b061b633d42ea3
Olga Miller <omel20072001@gmail.com>
2020-10-25 14:19:33 +0200

    Добавить страницу описание книги

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -7,6 +7,7 @@ namespace Store
     public interface IBookRepository
     {
         Book[] GetAllByIsbn(string isbn);
-        Book[] GetAllByTitleOrAuthor(string titleOrAuthor); 
+        Book[] GetAllByTitleOrAuthor(string titleOrAuthor);
+        Book GetById(int id);
     }
 }
