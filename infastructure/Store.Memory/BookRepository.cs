794c7ca0f545d670c7ca749da3471f4634532f04
Olga Miller <omel20072001@gmail.com>
2020-10-28 20:47:43 +0200

    Заполнение корзины

--- infastructure/Store.Memory/BookRepository.cs
+++ infastructure/Store.Memory/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Store.Memory
@@ -11,6 +12,14 @@ namespace Store.Memory
             new Book(2,"ISBN 12312-32132","M. Fowler","Refactoring", "bbbbb ",12.45m),
             new Book(3,"ISBN 12312-32133","B. Kernighan, D.Ritchie","C Programming Language"," ccccc" , 14.98m),
         };
+
+        public Book[] GetAllByIds(IEnumerable<int> bookIds)
+        {
+            var foundBooks = from book in books
+                             join bookId in bookIds on book.Id equals bookId
+                             select book;
+            return foundBooks.ToArray();
+        }
 
         public Book[] GetAllByIsbn(string isbn)
         {
